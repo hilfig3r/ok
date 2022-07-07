54a22b9a3063401e0c10cd35cce01545fd3a7f5f
Orca <106352155+CycloneHacks@users.noreply.github.com>
2022-07-06 20:51:14 -0400

    Update title.js

--- Pages/games/sleepingbeauty/src/js/ui/intro/title.js
+++ Pages/games/sleepingbeauty/src/js/ui/intro/title.js
@@ -1,14 +1,14 @@
 let node = document.createElement("div");
 node.classList.add("title");
 node.innerHTML =                                               
-".oPYo. 8                       o             \n" +
-"8      8                                     \n" +
-"`Yooo. 8 .oPYo. .oPYo. .oPYo. o8 odYo. .oPYo.\n" +
-"    `8 8 8oooo8 8oooo8 8    8  8 8' `8 8    8\n" + 
-"     8 8 8.     8.     8    8  8 8   8 8    8\n" +
-"`YooP' 8 `Yooo' `Yooo' 8YooP'  8 8   8 `YooP8\n" + 
-"                       8                    8\n" +
-"                       8                 ooP'\n" +
+".oPYo.  8                                    \n" +
+"8     8  8 8                                    \n" +
+"8     8  8  o.  .oPYo.  .oPYo.                    \n" +
+"8   ` 8  8      o        8oooo8              \n" + 
+"8     8  8      8.      8    8 8               \n" +
+"`YooP'           `Yooo'  8YooP' 88            \n" + 
+"                                               \n" +
+"                                             \n" +
 " .oPYo.                        o             \n" +
 " 8   `8                        8             \n" +
 "o8YooP' .oPYo. .oPYo. o    o  o8P o    o     \n" +
