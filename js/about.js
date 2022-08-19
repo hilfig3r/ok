adff4e9af614517cd5d2d5c1cc7ca8d75910b1c7
smile52 <104168698+smileman52@users.noreply.github.com>
2022-08-19 12:36:36 -0700

    Update and rename index.js to about.js

---
+++ js/about.js
@@ -0,0 +1,12 @@
+win = window.open();
+         win.document.body.style.margin = "0";
+         win.document.body.style.height = "100vh";
+         var iframe = win.document.createElement("iframe");
+         iframe.style.border = "none";
+         iframe.style.width = "100%";
+         iframe.style.height = "100%";
+         iframe.style.margin = "0";
+         iframe.referrerpolicy = "no-referrer";
+         iframe.allow = "fullscreen";
+         iframe.src = "https://cyclonehacks.github.io";
+         win.document.body.appendChild(iframe);
