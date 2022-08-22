10ba3cbd37bfbc1660c0a6c7f2978c771ff71f44
Orca <106352155+CycloneHacks@users.noreply.github.com>
2022-08-22 15:39:51 +0000

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,7 +1,7 @@
          win = window.open();
          win.document.body.style.margin = "0";
          win.document.body.style.height = "100vh";
-         win.document.body.appendChild(iframe);
+    
          var iframe = win.document.createElement("iframe");
          iframe.style.border = "none";
          iframe.style.width = "100%";
@@ -9,7 +9,10 @@
          iframe.style.margin = "0";
          iframe.referrerpolicy = "no-referrer";
          iframe.allow = "fullscreen";
+         win.document.body.appendChild(iframe);
          iframe.src = "https://cyclonehacks.github.io";
-window.close()
+
+         window.close()
+
 
 
